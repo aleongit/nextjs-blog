1f6f9bf48af820fce8265dca1f5b42d8ad128a79
aleongit <100855283+aleongit@users.noreply.github.com>
2022-07-07 16:04:16 +0200

    data-fetching

--- README.md
+++ README.md
@@ -1,12 +1,18 @@
 This is a starter template for [Learn Next.js](https://nextjs.org/learn).
 
+# Create next app
+
 - npx create-next-app nextjs-blog --use-npm --example "https://github.com/vercel/next-learn/tree/master/basics/learn-starter"
 
+# Run
 - cd nextjs-blog
 - npm run dev
 - http://localhost:3000/
+
+# Summary
 
 - https://nextjs.org/learn/basics/create-nextjs-app
 - https://nextjs.org/learn/basics/navigate-between-pages
 - https://nextjs.org/learn/basics/assets-metadata-css
 - https://nextjs.org/learn/basics/data-fetching
+- TODO https://nextjs.org/learn/basics/dynamic-routes
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,59 @@
-import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout';
-import utilStyles from '../styles/utils.module.css';
+import Head from "next/head"
+import Link from "next/link"
+import Layout, { siteTitle } from "../components/layout"
+import utilStyles from "../styles/utils.module.css"
 
-export default function Home() {
+import { getSortedPostsData } from "../lib/posts"
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hi! I'm <b>paca</b>! I'm a software engineer and a translator (English/Japanese). You can contact me on Twitter.</p>
         <p>
-          (This is a sample website - you'll be building a site like this on{' '}
+          Hi! I'm <b>paca</b>! I'm a software engineer and a translator
+          (English/Japanese). You can contact me on Twitter.
+        </p>
+        <p>
+          (This is a sample website - you'll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+      </section>
+
+      <section>
+        <p className="title">
+        &#128264; Read{" "}
+          <Link href="/posts/first-post">
+            <a>this the first post page!</a>
+          </Link>
+        </p>
+      </section>
+
+      {/* Add this <section> tag below the existing <section> tag */}
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
